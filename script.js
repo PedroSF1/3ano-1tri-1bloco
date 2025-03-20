a781675a4e7b201d83df4b9622ca20df9abad9fc
PedroSF1 <143829846+PedroSF1@users.noreply.github.com>
2025-03-20 14:21:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+    botaoDeAcessibilidade.addEventListener('click', function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+    })
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte')
+
+    const alternaContraste = documen.getElementById('alterna-contraste')
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
@@ -13,6 +23,10 @@ document.addEventListener('DOMContentLoaded', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
+    })
+
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
     })
 })
 
